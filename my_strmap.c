4c170f4844f08b578da31c4ad45c90692012c2e9
Emmett Butler <emmett.butler321@gmail.com>
2012-11-19 17:10:10 -0500

    typo

--- my_strmap.c
+++ my_strmap.c
@@ -107,7 +107,7 @@ void sm_dealloc(strMap *map){
     Bucket *bucket = map->buckets, *tmp_bucket;
     // deallocate the collision trees
     for(i = 0; i < map->count; i++){
-        tmp_bucket = bucket + sizeof(bucket*);
+        tmp_bucket = bucket + sizeof(Bucket*);
         rb_dealloc(bucket->strings);
         bucket = tmp_bucket;
     }
